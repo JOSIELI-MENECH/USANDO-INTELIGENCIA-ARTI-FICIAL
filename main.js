46578491e241031d9d54f15c239d8e9c48a492b7
JOSIELI-MENECH <josieli.menech@escola.pr.gov.br>
2025-09-02 11:18:55 -0300

    dia 2 de setembro

---
+++ main.js
@@ -0,0 +1,32 @@
+const caixaPrincipal = document.querySelector (".caixa-principal");
+const caixaPerguntas = document.querySelector (".caixa-perguntas");
+const caixaAlternativas = document.querySelector (".caixa-alternativas");
+const caixaResultado = document.querySelector (".caixa-resultado");
+const textoResultado = document.querySelector (".texto-resultado");
+
+
+const perguntas = {
+    enunciado: "pergunta 1",
+    alternativas [
+        "Alternativa 1",
+        "Alternativa 2",
+    ]
+}
+
+const perguntas = {
+    enunciado: "pergunta 2",
+    alternativas [
+        "Alternativas 1",
+        "Alternativas 2",
+    ]
+}
+
+const perguntas = {
+    enunciado: "pergunta 3",
+    alternativas [
+        "Alternativa 1",
+        "Alternativa 2",
+    ]
+}
+
+
