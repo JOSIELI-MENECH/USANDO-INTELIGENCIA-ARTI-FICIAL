155f1d7a80aa2d11d94b5a364d9af991cc5a3898
JOSIELI-MENECH <josieli.menech@escola.pr.gov.br>
2025-09-24 11:16:36 -0300

    dia 24 de setembro

--- main.js
+++ main.js
@@ -6,67 +6,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "No caminho de volta para casa você se depara com , o que você faz?",
+        enunciado: "Você resolve problemas levando em maior consideração o racional ou a emoção?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
+                texto: "O racional.",
+                afirmacao: "Você possui o lado racional mais forte, levando em consideração o pensamento apurado antes de decisões."
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "A emoção.",
+                afirmacao: "Você possui o lado emocional mais forte, levando em consideração a expressão de sentimentos durante as decisões."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Você possui maiores habilidades comunicativas ou maiores habilidades para resolver problemas lóǵicos?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Habilidades para resolver problemas lógicos",
+                afirmacao: "Sua velocidade de pensamento é mais apurada em questões lógicas e de racíocinio."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Habilidades comunicativas.",
+                afirmacao: "Sente facilidade em conquistar espaço através de diálogos e expressões de ideias."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Você prefere atividades que explorem mais o seu lado criativo ou atividades que possuam uma prática aplicada?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Atividades com prática aplicada.",
+                afirmacao: "Prefere atividades com práticas predestinas no cotidiano."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Atividades que explorem a criatividade.",
+                afirmacao: "Prefere atividades que explorem o lado criativo e expressivo da mente."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Você tem facilidade em trabalhar em grupos ou tem melhor desempenho em trabalhos indivíduais?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Maior facilidade em trabalhos indíviduais.",
+                afirmacao: "Possui clareza em trabalhar sozinho."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Maior facilidade em trabalhos em grupo.",
+                afirmacao: "Possui desenvoltura em trabalhar com outras pessoas."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Qual área você escolheria para seguir profissionalmente?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Matemática e suas tecnologias",
+                afirmacao: "Portanto você se enquadra nas características de uma pessoa de exatas."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Linguagens e suas tecnologias.",
+                afirmacao: "Portanto você se enquadra nas características de uma pessoa de humanas "
             }
         ]
     },
